d87f6997949e7d09c2e810a76db72484cdd93e10
WinterfellCoder <alex.hernandezp89@gmail.com>
2026-06-21 09:35:34 +0200

    Update results.js

--- results.js
+++ results.js
@@ -3,54 +3,54 @@ const RESULTS = {
   thirdPlace: [],
   groupMatches: { "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
-      "Mexico__South Korea": { "home": -1, "away": -1 },
+      "Mexico__South Korea": { "home": 1, "away": 0 },
       "Mexico__Czech Republic": { "home": -1, "away": -1 },
       "South Africa__South Korea": { "home": -1, "away": -1 },
-      "South Africa__Czech Republic": { "home": -1, "away": -1 },
+      "South Africa__Czech Republic": { "home": 1, "away": 1 },
       "South Korea__Czech Republic": { "home": 2, "away": 1 }
     },
     "B": {
       "Canada__Switzerland": { "home": -1, "away": -1 },
-      "Canada__Qatar": { "home": -1, "away": -1 },
+      "Canada__Qatar": { "home": 6, "away": 0 },
       "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 },
-      "Qatar__Switzerland": { "home": 1, "away": 1 },
-      "Switzerland__Bosnia & Herzegovina": { "home": -1, "away": -1 },
+      "Switzerland__Qatar": { "home": 1, "away": 1 },
+      "Switzerland__Bosnia & Herzegovina": { "home": 4, "away": 1 },
       "Qatar__Bosnia & Herzegovina": { "home": -1, "away": -1 }      
     },
     "C": {
-      "Brazil__Haiti": { "home": -1, "away": -1 },
+      "Brazil__Haiti": { "home": 3, "away": 0 },
       "Brazil__Morocco": { "home": 1, "away": 1 },
       "Brazil__Scotland": { "home": -1, "away": -1 },
       "Haiti__Morocco": { "home": -1, "away": -1 },
       "Haiti__Scotland": { "home": 0, "away": 1 },
-      "Morocco__Scotland": { "home": -1, "away": -1 }
+      "Morocco__Scotland": { "home": 0, "away": 1 }
     },
     "D": {
-      "Turkey__Paraguay": { "home": -1, "away": -1 },
+      "Turkey__Paraguay": { "home": 0, "away": 1 },
       "Australia__Turkey": { "home": 2, "away": 0 },
       "Turkey__USA": { "home": -1, "away": -1 },
       "Paraguay__Australia": { "home": -1, "away": -1 },
       "USA__Paraguay": { "home": 4, "away": 1 },
-      "Australia__USA": { "home": -1, "away": -1 }
+      "Australia__USA": { "home": 2, "away": 0 }
     },
     "E": {
       "Curaçao__Ecuador": { "home": -1, "away": -1 },
-      "Germany__Curaçao": { "home": 7, "away": 1 },
+      "Curaçao__Germany": { "home": 7, "away": 1 },
       "Curaçao__Ivory Coast": { "home": -1, "away": -1 },
       "Ecuador__Germany": { "home": -1, "away": -1 },
-      "Ivory Coast__Ecuador": { "home": 1, "away": 0 },
+      "Ecuador__Ivory Coast": { "home": 1, "away": 0 },
       "Germany__Ivory Coast": { "home": -1, "away": -1 }
     },
     "F": {
-      "Netherlands__Japan": { "home": 2, "away": 2 },
+      "Japan__Netherlands": { "home": 2, "away": 2 },
       "Japan__Tunisia": { "home": -1, "away": -1 },
       "Japan__Sweden": { "home": -1, "away": -1 },
       "Netherlands__Tunisia": { "home": -1, "away": -1 },
       "Netherlands__Sweden": { "home": -1, "away": -1 },
-      "Sweden__Tunisia": { "home": 5, "away": 1 }
+      "Tunisia__Sweden": { "home": 5, "away": 1 }
     },
      "G": {
-      "Belgium__Egypt": { "home": 1, "away": 1 },
+      "Egypt__Belgium": { "home": 1, "away": 1 },
       "Egypt__Iran": { "home": -1, "away": -1 },
       "Egypt__New Zealand": { "home": -1, "away": -1 },
       "Belgium__Iran": { "home": -1, "away": -1 },
@@ -59,7 +59,7 @@ const RESULTS = {
     },
     "H": {
       "Uruguay__Spain": { "home": -1, "away": -1 },
-      "Saudi Arabia__Uruguay": { "home": 1, "away": 1 },
+      "Uruguay__Saudi Arabia": { "home": 1, "away": 1 },
       "Uruguay__Cape Verde": { "home": -1, "away": -1 },
       "Spain__Saudi Arabia": { "home": -1, "away": -1 },
       "Spain__Cape Verde": { "home": 0, "away": 0 },
@@ -78,7 +78,7 @@ const RESULTS = {
       "Argentina__Algeria": { "home": 3, "away": 0 },
       "Argentina__Austria": { "home": -1, "away": -1 },
       "Jordan__Algeria": { "home": -1, "away": -1 },
-      "Austria__Jordan": { "home": 3, "away": 1 },
+      "Jordan__Austria": { "home": 3, "away": 1 },
       "Algeria__Austria": { "home": -1, "away": -1 }
     },
     "K": {
